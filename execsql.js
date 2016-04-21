a1111e94f31e10ed0c1ac026c1ca8cd028978e8c
wh <1010127369@qq.com>
2016-04-21 19:07:29 +0800

    add exesql.js 可以执行mysql脚本

---
+++ execsql.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+var config = require('./config');
+var sqlexecutor = require('./sqlexecutor')
+var sql = process.argv[2];
+
+DoInstall();
+function DoInstall() {
+    if(sql !==null && sql !==undefined)
+    {
+    }
+    else
+    {
+        sql = "select * from bi_posts";
+    }
+    
+    //sqlexecutor.ExecSql('USE r7gs53056x0gq8dq;');
+    sqlexecutor.ExecSql(sql, undefined,
+        function (err, rows) {
+            if (err) log(err, 3);
+            else {  //
+                console.log(rows);
+                console.log('等待退出...');
+                setTimeout(function () {
+                    process.exit();
+                }, 3000);
+            }
+        }
+        );
+
+
+}
--- test-mysql.js
+++
@@ -1,9 +0,0 @@
-var mysql      = require('mysql');
-var config     = require('./config');
-var sqlexecutor = require('./sqlexecutor')
-DoInstall();
-function DoInstall()
-{
-    sqlexecutor.ExecSql('USE r7gs53056x0gq8dq;');
-    sqlexecutor.ExecSql('select * from bi_posts;');
-}
