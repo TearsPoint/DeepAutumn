1717ba0a8e30a84b7eba82646783b46ff82ffa3d
王衡 <wangheng920411@163.com>
2016-07-25 21:14:19 +0800

    **
    **

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ var config =
     db_host     : '172.18.99.61', /* Database IP */
     db_port     : 3306,        /* Database Port */
     db_user     : 'sa',        /* Database Username */
-    db_password : '123mysql',  /* Database Password */
+    db_password : '****',  /* Database Password */
     db_database : 'coredb',    /* Database Name */
     db_engine   : 'InnoDB',
     blog_name   :  'DeepAutumn',
