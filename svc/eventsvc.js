03089efa1bacd94879a99e5f2dd1bda5f7b8b1b6
TearsPoint <1010127369@qq.com>
2016-08-23 15:10:43 +0800

    wx校验

--- svc/eventsvc.js
+++ svc/eventsvc.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var runtime = require('../runtime');
 var sqlexecutor = require('../sqlexecutor');
+var url = require('url');
 
 function push(router) {
     router.get('/eventsvc', function (req, res) {
@@ -8,6 +9,13 @@ function push(router) {
         
         res.send('{"eventsvc":1}');
     });
+
+    router.get('/wxcheck', function (req, res) {
+        var param = url.parse(req.url, true).query;
+        res.send(param.echostr);
+    });
 }
 
 module.exports.push = push;
+
+//GET /svc/wxcheck?signature=8cfa79918f69a736850d92e024c05660781cc1fa&echostr=717501845316042663&timestamp=1471931942&nonce=2080273500 200 4.394 ms - 9
